e591e35d559ee6807fff44b23a21cdaa576886f9
webcodelab <webcodelabb@gmail.com>
2025-05-29 23:19:24 +0100

    Update project config and deployment scripts

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+// CHANGE THIS IMPORT:
+import { HashRouter, Routes, Route } from "react-router-dom"; // Changed BrowserRouter to HashRouter
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 
@@ -13,13 +14,14 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      {/* CHANGE THIS COMPONENT: */}
+      <HashRouter> {/* Changed BrowserRouter to HashRouter */}
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
