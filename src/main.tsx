8a96f395b1d89dc1ba5cd7719bd926aea5944501
webcodelab <webcodelabb@gmail.com>
2025-05-29 22:40:36 +0100

    Deploy site

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,5 @@
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'   // import BrowserRouter
 import App from './App.tsx'
 import './index.css'
 
-createRoot(document.getElementById("root")!).render(
-  <BrowserRouter basename="/Brew-Bean-Modern-Coffee-Shop-Website/">
-    <App />
-  </BrowserRouter>
-);
+createRoot(document.getElementById("root")!).render(<App />);
