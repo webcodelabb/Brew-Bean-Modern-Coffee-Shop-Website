f576da3acf1f883183bebd77f0faf44fd2ff7bc9
Webcodelab <141560205+webcodelabb@users.noreply.github.com>
2025-06-08 16:40:25 +0100

    Update Gallery.tsx

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -5,35 +5,35 @@ const Gallery = () => {
 
   const galleryImages = [
     {
-      src: "/assets/Coffee-shop-interior.png",
+      src: "https://cdn.shopify.com/s/files/1/0491/2165/6981/files/Eco-Friendly_and_Sustainable_480x480.webp?v=1733402305",
       alt: "Coffee shop interior"
     },
     {
-      src: "/assets/Barista-making-coffee.png",
+      src: "/https://images.stockcake.com/public/1/0/3/103f12d4-962f-454f-8e7a-3337442069ff_large/barista-making-coffee-stockcake.jpg",
       alt: "Barista making coffee"
     },
     {
-      src: "/assets/Coffee-beans.png",
+      src: "https://www.nescafe.com/au/sites/default/files/2024-04/Untitled-5%20copy_6_0.jpg",
       alt: "Coffee beans"
     },
     {
-      src: "/assets/Cozy-seating-area.png",
+      src: "https://www.marthastewart.com/thmb/OEeMUozadwnFNdVjcDNHthLUlMs=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/ms-cozy-living-room-ideas-michael-hunter-1-c7e9c09909954856a92206adf3b2dfbf.JPG",
       alt: "Cozy seating area"
     },
     {
-      src: "/assets/Latte-art.png",
+      src: "https://cdn.greenplantation.com/2020/07/latte-art-ruze.jpg",
       alt: "Latte art"
     },
     {
-      src: "/assets/Coffee-roasting.png",
+      src: "https://mtpak.coffee/wp-content/uploads/2021/02/image2.jpg",
       alt: "Coffee roasting"
     },
     {
-      src: "/assets/Fresh-pastries.png",
+      src: "https://www.bhg.com/thmb/7dROILyl6Q6ZtiKBbe8B_CgC_II=/2250x0/filters:no_upscale():strip_icc()/Lady-Locks-Recipe-6750995-fece434185e547f985ecb87ef4444e48.jpg",
       alt: "Fresh pastries"
     },
     {
-      src: "/assets/Coffee-shop-atmosphere.png",
+      src: "https://jukeboxy-media.s3.amazonaws.com/blog/wp-content/uploads/2022/02/19102049/coffee-shops-music-provider.jpg",
       alt: "Coffee shop atmosphere"
     }
   ];
